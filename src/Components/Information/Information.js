f0708fd1a23306b9ce61f8734a2854932e83eb92
William Won <Wiwon123@Williams-MacBook-Pro.local>
2018-05-05 02:54:45 -0500

    may 5

--- src/Components/Information/Information.js
+++ src/Components/Information/Information.js
@@ -31,47 +31,51 @@ class Information extends Component {
         </div>
         <div className="infoInput__container">
           <div className="infoInput__intro">
-            Let us get to know you a little better
+            Let us get to know you a little better...
           </div>
-          <input
-            className="infoInput username"
-            placeholder="Desired Display Name"
-            type="text"
-            onChange={e => updateUserName(e.target.value)}
-          />
-          <input
-            className="infoInput email"
-            placeholder="Primary Email"
-            type="text"
-            onChange={e => updateEmail(e.target.value)}
-          />
-          <input
-            className="infoInput address"
-            placeholder="Home Address"
-            type="text"
-            onChange={e => updateAddress(e.target.value)}
-          />
-          <input
-            className="infoInput city"
-            placeholder="City"
-            type="text"
-            onChange={e => updateCity(e.target.value)}
-          />
-          <input
-            className="infoInput state"
-            placeholder="State"
-            type="text"
-            onChange={e => updateMyState(e.target.value)}
-          />
-          <input
-            className="infoInput zipcode"
-            placeholder="Zipcode"
-            type="number"
-            onChange={e => updateZip(e.target.value)}
-          />
-          <Link to="/confirm">
-            <button>Complete</button>
-          </Link>
+          <div className="infoInput__sections">
+            <input
+              className="infoInputArea username"
+              placeholder="Desired Display Name"
+              type="text"
+              onChange={e => updateUserName(e.target.value)}
+            />
+            <input
+              className="infoInputArea email"
+              placeholder="Primary Email"
+              type="text"
+              onChange={e => updateEmail(e.target.value)}
+            />
+            <input
+              className="infoInputArea address"
+              placeholder="Home Address"
+              type="text"
+              onChange={e => updateAddress(e.target.value)}
+            />
+            <input
+              className="infoInputArea city"
+              placeholder="City"
+              type="text"
+              onChange={e => updateCity(e.target.value)}
+            />
+            <input
+              className="infoInputArea state"
+              placeholder="State"
+              type="text"
+              onChange={e => updateMyState(e.target.value)}
+            />
+            <input
+              className="infoInputArea zipcode"
+              placeholder="Zipcode"
+              type="number"
+              onChange={e => updateZip(e.target.value)}
+            />
+          </div>
+          <div className="infoInput__confirm__container">
+            <Link to="/confirm">
+              <button className="infoInput__confirm">Complete</button>
+            </Link>
+          </div>
         </div>
       </div>
     );
